7996f506f2371517a768bc7453dd66a699863e54
shriyansh5776 <chotiashriyansh9@gmail.com>
2026-06-08 21:43:01 +0530

    Add route to get all schedules

--- backend/routes/schedule_routes/Schedule.routes.js
+++ backend/routes/schedule_routes/Schedule.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import userAuth from "../../middleware/auth.middleware.js";
 import isRailwayEmployee from "../../middleware/railwayEmployee.middleware.js";
-import { createSchedule, deleteSchedule, getScheduleById, getScheduleByTrainId, updateSchedule } from "../../controllers/schedule/Schedule.controller.js";
+import { createSchedule, deleteSchedule, getScheduleById, getScheduleByTrainId, updateSchedule ,getAllSchedules } from "../../controllers/schedule/Schedule.controller.js";
 
 const scheduleRouter =Router()
-
+scheduleRouter.get("/", userAuth, getAllSchedules);
 scheduleRouter.post("/create",userAuth,isRailwayEmployee,createSchedule)
 scheduleRouter.get("/train/:trainId",userAuth,isRailwayEmployee,getScheduleByTrainId)
 scheduleRouter.get("/:id",userAuth,isRailwayEmployee,getScheduleById)
